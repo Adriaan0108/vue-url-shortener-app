72274810612b3357e47aa4f316c38298654e651e
Adriaan Pienaar <112950444+Adriaan0108@users.noreply.github.com>
2025-09-07 11:40:33 +0200

    Fix axios token undefined error

--- src/api/axios.js
+++ src/api/axios.js
@@ -9,14 +9,13 @@ export const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config) => {
     const auth = useAuthStore();
-    if (auth.user.token) {
-      config.headers.Authorization = `Bearer ${auth.user.token}`;
+    const token = auth.user?.token; // safe optional chaining
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default axiosInstance;
